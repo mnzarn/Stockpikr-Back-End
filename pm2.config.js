b1e4840668e4d90c30735d46aef12650af3c51c4
ducphamle2 <ducphamle212@gmail.com>
2024-01-26 12:39:27 -0800

    feat: add pm2 config & cd github action

---
+++ pm2.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "stockpikr",
+      script: "./index.ts",
+      cwd: "~/stockpikr/source"
+    }
+  ],
+  deploy: {
+    production: {
+      key: "~/.ssh/id_rsa",
+      user: "duclepham",
+      host: ["172.200.217.33"],
+      ref: "origin/main",
+      repo: "git@github.com:Ring-A-Bell/StockPikr.git",
+      path: "~/stockpikr",
+      "post-setup": "cp ~/.env ~/stockpikr/source/.env",
+      "pre-deploy": "yarn"
+    }
+  }
+};
