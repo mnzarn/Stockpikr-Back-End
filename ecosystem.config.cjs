c335c0a5baef649934db0c57547a59a8b3904411
ducphamle2 <ducphamle212@gmail.com>
2024-01-26 17:24:52 -0800

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,22 @@
 module.exports = {
+  apps: [
+    {
+      name: "stockpikr",
+      script: "./index.ts",
+      intepreter: "tsx"
+    }
+  ],
   deploy: {
     production: {
       key: "~/.ssh/stockpikr_id_rsa",
       user: "duclepham",
       host: ["172.200.217.33"],
-      ref: "origin/main",
+      ref: "origin/cicd/pipeline",
       repo: "git@github.com-stockpikr:Ring-A-Bell/StockPikr.git",
       path: "/home/duclepham/stockpikr",
       "pre-setup": "npm install -g tsx && rm -rf ~/stockpikr",
       "post-setup": "cp ~/.env ~/stockpikr/source/.env",
-      "post-deploy": "cd ~/stockpikr/source/ && yarn && pm2 startOrRestart --interpreter tsx --name stockpikr index.ts"
+      "post-deploy": "cd ~/stockpikr/source/ && yarn && pm2 startOrRestart ecosystem.config.cjs index.ts"
     }
   }
 };
