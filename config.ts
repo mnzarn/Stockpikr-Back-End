1896e7d8157d8a9717a1fc378499227e59b3cafa
Aditya Ganti <adi10rox@gmail.com>
2024-03-03 09:22:50 -0800

    change port to 80 for HTTP access on Azure

--- config.ts
+++ config.ts
@@ -3,12 +3,12 @@ dotenv.config();
 
 export const config = {
   FMP_API_KEY: process.env.FMP_API_KEY,
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 80,
   DB_CONNECTION_STRING:
     process.env.DB_CONNECTION_STRING || "mongodb+srv://test:test@stockpikr.zrmjvdi.mongodb.net/Users",
   MONGO_CONN_TIMEOUT: 1000,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   GOOGLE_CALLBACK_URL: process.env.GOOGLE_CLIENT_CALLBACK,
-  NODE_ENV: process.env.NODE_ENV || "production",
+  NODE_ENV: process.env.NODE_ENV || "production"
 };
