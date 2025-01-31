085c77e4e01d3774899f257aa5781928acdb6db1
szayadi <szayadi@seattleu.edu>
2025-01-30 21:15:12 -0800

    updating servers to test connection

--- config.ts
+++ config.ts
@@ -5,7 +5,8 @@ export const config = {
   FMP_API_KEY: process.env.FMP_API_KEY,
   PORT: process.env.PORT || 80,
   DB_CONNECTION_STRING:
-    process.env.DB_CONNECTION_STRING || "mongodb+srv://test:test@stockpikr.zrmjvdi.mongodb.net/Users",
+    process.env.DB_CONNECTION_STRING ||
+    "mongodb+srv://sammyzayadi1:3jC8BPiIGn8RXc8m@stockpikrcluster.d4itd.mongodb.net/?retryWrites=true&w=majority&appName=stockpikrcluster",
   MONGO_CONN_TIMEOUT: 1000,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,11 +9,11 @@ module.exports = {
   deploy: {
     production: {
       key: "~/.ssh/stockpikr_id_rsa",
-      user: "duclepham",
-      host: ["172.200.217.33"],
+      user: "sammyzayadi1",
+      host: ["73.193.81.238"],
       ref: "origin/main",
-      repo: "git@github.com-stockpikr:Ring-A-Bell/StockPikr.git",
-      path: "/home/duclepham/stockpikr",
+      repo: "git@github.com:mnzarn/Stockpikr-Back-End.git",
+      path: "C:/Users/ajani/Stockpikr-Back-End",
       "pre-setup": "npm install -g tsx && rm -rf ~/stockpikr",
       "post-setup": "cp ~/.env ~/stockpikr/source/.env",
       "post-deploy": "cd ~/stockpikr/source/ && yarn && pm2 startOrRestart ecosystem.config.cjs --env production"
