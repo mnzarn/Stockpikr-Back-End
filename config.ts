adecb64bbdf85f42b5d5bcee20e5f72fb15b830b
Arnold Manzi <93343042+mnzarn@users.noreply.github.com>
2025-02-18 17:19:28 -0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -9,6 +9,6 @@ export const config = {
   MONGO_CONN_TIMEOUT: 1000,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CLIENT_CALLBACK,
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CLIENT_URL,
   NODE_ENV: process.env.NODE_ENV || "production"
 };
