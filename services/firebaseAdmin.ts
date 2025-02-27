dfbeb791ed86af9554ff569ed607d8a658de508b
Harpreet Sangha <100447473+hasangha04@users.noreply.github.com>
2025-02-26 17:12:25 -0800

    Update firebaseAdmin.ts to work with Base64 instead of a string

--- services/firebaseAdmin.ts
+++ services/firebaseAdmin.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
 import admin from "firebase-admin";
 
-const rawServiceAccount = process.env.FIREBASE_SERVICE_ACCOUNT || "{}";
-const serviceAccount = JSON.parse(rawServiceAccount);
+dotenv.config();
 
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+const base64Encoded = process.env.FIREBASE_SERVICE_ACCOUNT || "";
+const jsonString = Buffer.from(base64Encoded, "base64").toString("utf-8");
+
+const serviceAccount = JSON.parse(jsonString);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
