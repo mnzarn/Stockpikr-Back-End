a8fb373b95267e907f544c31c277f1ea1554d091
Harpreet Sangha <hasangha04@gmail.com>
2025-05-14 12:57:09 -0700

    Added scalability to the cron job to fetch stock quotes

--- index.ts
+++ index.ts
@@ -30,10 +30,10 @@ const host = "0.0.0.0";
     console.log(`⚡️[server]: Server is running at http://${host}:${port}`);
   });
   const cronFmp = new CronFmp(latestStockInfoModel, userModel, watchlistModel);
-  // we cron every 5 mins, but only fetch data from fmp api iff there's no tickers stored or the tickers are more than a day old
-  // usually, a cron job lib is not suitable for async tasks because it would potentially causing race conditions if the task takes more than 5 mins to run
-  // however, our logic should not take 5 mins. If it does -> there's something wrong
-  cron.schedule("* 5 * * * *", cronFmp.fetchOrUpdateLatestStocks, {
+  // we cron every 10 mins, but only fetch data from fmp api if there's no tickers stored or the tickers are more than a day old
+  // usually, a cron job lib is not suitable for async tasks because it would potentially causing race conditions if the task takes 
+  // more than 5 mins to run however, our logic should not take 5 mins. If it does -> there's something wrong
+  cron.schedule("*/5 * * * *", cronFmp.fetchOrUpdateLatestStocks, {
     runOnInit: true
   });
 })();
